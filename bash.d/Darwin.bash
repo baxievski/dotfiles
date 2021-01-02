44da442d50609d1f1a0763b52a6226bbc82201bd
Bojan Adzievski <bojana@tabtale.com>
2021-01-02 09:19:04 +0100

    wip

--- bash.d/Darwin.bash
+++ bash.d/Darwin.bash
@@ -1,4 +1,4 @@
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
-[[ ! -f /usr/libexec/java_home ]] || export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+# [[ ! -f /usr/libexec/java_home ]] || export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
